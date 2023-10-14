6692404b79d40435b556b5bcbba9bb14a22192b4
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-10-14 18:32:24 +0530

    Update Bubble Sort.cpp

--- SORTING/Bubble Sort.cpp
+++ SORTING/Bubble Sort.cpp
@@ -1,1 +1,17 @@
+// BUBBLE SORT
+vector<int> sortArray(vector<int>& nums) {
+    // swapping for wrong order
+    int n = nums.size();
+    vector<int> res(nums.begin(), nums.end());
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = ; j < n-i; j++) {
+            if(res[j] < res[j-1]) {
+                swap(res[j], res[j-1]);
+            }
+        }
+    }
+    
+    return res;
+}
 
