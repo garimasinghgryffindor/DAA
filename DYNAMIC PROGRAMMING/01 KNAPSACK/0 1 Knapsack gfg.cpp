981cf683c201a5551b2ed42d0810df8803b6e57e
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-10-18 13:51:22 +0530

    Update 0 1 Knapsack gfg.cpp

--- DYNAMIC PROGRAMMING/01 KNAPSACK/0 1 Knapsack gfg.cpp
+++ DYNAMIC PROGRAMMING/01 KNAPSACK/0 1 Knapsack gfg.cpp
@@ -1,17 +1,22 @@
-int knapSack(int W, int wt[], int val[], int n) 
-{ 
-   // using recursion
-   // take or not take
-   if(n <= 0) return 0;
-   if(W == 0) return 0;
-   // either you will take nth item 
-   int res = 0;
-   if(W-wt[n-1] >= 0) {
-       res = max(res, val[n-1] + knapSack(W-wt[n-1], wt, val, n-1));
-   }
-   
-   res = max(res, knapSack(W, wt, val, n-1));
-   return res;
-}
+int recur(int W, int wt[], int val[], int i) {
+     // take or not take
+    if(i < 0) return 0;
+    if(W <= 0) return 0;
+    // either you will take nth item 
+    int res = 0;
+    if(W-wt[i] >= 0) {
+        res = max(res, val[i] + recur(W-wt[i], wt, val, i-1));
+    }
+    
+    res = max(res, recur(W, wt, val, i-1));
+    return res;
+ }
+ 
+ int knapSack(int W, int wt[], int val[], int n) 
+ { 
+    // using recursion
+    return recur(W, wt, val, n-1);
+ }
+
 
 
