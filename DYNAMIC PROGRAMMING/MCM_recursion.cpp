33fd9f7319360a77e981dd4c81fcb86d101075c0
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-10-18 16:06:02 +0530

    Update MCM_recursion.cpp

--- DYNAMIC PROGRAMMING/MCM_recursion.cpp
+++ DYNAMIC PROGRAMMING/MCM_recursion.cpp
@@ -1,55 +1,17 @@
-//MATRIX CHAIN MULTIPLICATION
-//RECURSION
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int MCM(vector <int> p , int i , int j)
-{
-    if(i==j)
-        return 0;
+// using RECURSION
+    int recur(int arr[], int i, int j) {
+        if(i == j) return 0;
+        
+        int mn = INT_MAX;
+        for(int k = i; k < j; k++) {
+            int temp = arr[i-1]*arr[k]*arr[j];
+            mn = min(mn, temp + recur(arr,i,k) + recur(arr, k+1, j));
+        }
+        
+        return mn;
+    }
     
-    int k;
-    int min=INT_MAX;
-    int count;
-
-    for(k=i ; k<j ; k++)
+    int matrixMultiplication(int N, int arr[])
     {
-        count = MCM(p,i,k) + MCM(p,k+1,j) + p[i-1]*p[k]*p[j];
-        if(count<min)
-        {
-            min=count;
-        }
+        return recur(arr, 1, N-1);
     }
-
-    return min;
-}
-
-int main()
-{  
-    int n , x , y; 
-    cout<<"Enter the number of matrices:  ";
-    cin>>n;
-
-    vector <int> p(n+1);
-
-    for(int i=0 ; i<n ; i++)
-    {
-        cout<<"Enter the dimension : ";
-        cin>>x>>y;
-        p[i]=x;
-        if(i==n-1)
-        {
-            p[i+1]=y;
-        }
-    }
-
-    int count = MCM(p,1,n);
-
-    cout<<"Minimum number of operations:  "<<count<<endl;
-
-    return 0;
-}
