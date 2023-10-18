b228a71b041e5efd5b2660ab0c9fe64ec8ff107c
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-10-18 13:57:43 +0530

    Update 0 1 Knapsack gfg.cpp

--- DYNAMIC PROGRAMMING/01 KNAPSACK/0 1 Knapsack gfg.cpp
+++ DYNAMIC PROGRAMMING/01 KNAPSACK/0 1 Knapsack gfg.cpp
@@ -1,3 +1,4 @@
+// RECURSION
 int recur(int W, int wt[], int val[], int i) {
      // take or not take
     if(i < 0) return 0;
@@ -18,6 +19,35 @@ int recur(int W, int wt[], int val[], int i) {
     // using recursion
     return recur(W, wt, val, n-1);
  }
+
+
+// MEMOIZATION
+int recur(vector<vector<int>>&mem,int W, int wt[], int val[], int i) {
+   // take or not take
+   if(mem[i+1][W] != -1) return mem[i+1][W];
+   
+  if(i < 0) { return mem[i+1][W] = 0; }
+  if(W <= 0) { return mem[i+1][W] = 0; }
+  
+  // either you will take nth item 
+  int res = 0;
+  if(W-wt[i] >= 0) {
+      res = max(res, val[i] + recur(mem, W-wt[i], wt, val, i-1));
+  }
+  
+  // or not
+  res = max(res, recur(mem, W, wt, val, i-1));
+  return { mem[i+1][W] = res };
+}
+
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+  // using memoization
+  vector<vector<int>> mem(n+1, vector<int>(W+1,-1));
+  return recur(mem,W, wt, val, n-1);
+}
+
+
 
 
 
