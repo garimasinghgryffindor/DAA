ed89de5e41ffe0134a008e4ea4a738c3f384c8d8
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-10-18 14:29:25 +0530

    Update lcs.cpp

--- DYNAMIC PROGRAMMING/LONGEST COMMON SUBSEQUENCE/lcs.cpp
+++ DYNAMIC PROGRAMMING/LONGEST COMMON SUBSEQUENCE/lcs.cpp
@@ -5,44 +5,39 @@ using namespace std;
 
 
 // RECURSION
-//int lcs(int x, int y, string s1, string s2)
-//{
-//    if(x==0 || y==0)
-//        return 0;
-//    if(s1[x-1] == s2[y-1])
-//        return 1 + lcs(x-1,y-1,s1,s2);
-//    else 
-//        return max(lcs(x-1,y,s1,s2), lcs(x,y-1,s1,s2));
-//}
-
-
+int lcs(int x, int y, string s1, string s2)
+{
+   if(x==0 || y==0)
+       return 0;
+   if(s1[x-1] == s2[y-1])
+       return 1 + lcs(x-1,y-1,s1,s2);
+   else 
+       return max(lcs(x-1,y,s1,s2), lcs(x,y-1,s1,s2));
+}
 
 
 // MEMOIZATION
-//int lcsUtils(int x,int y,string s1,string s2,vector<vector<int> >&dp) {
-//    if(x==0 || y==0)
-//        return 0;
-//    if(dp[x][y] != -1)
-//        return dp[x][y];
+int lcsUtils(int x,int y,string s1,string s2,vector<vector<int> >&dp) {
+   if(x==0 || y==0)
+       return 0;
+   if(dp[x][y] != -1)
+       return dp[x][y];
         
-//    if(s1[x-1] == s2[y-1])
-//        return (dp[x][y] = 1 + lcsUtils(x-1,y-1,s1,s2,dp));
-//    else 
-//        return (dp[x][y] = max(lcsUtils(x-1,y,s1,s2,dp), lcsUtils(x,y-1,s1,s2,dp)));
-//}
+   if(s1[x-1] == s2[y-1])
+       return (dp[x][y] = 1 + lcsUtils(x-1,y-1,s1,s2,dp));
+   else 
+       return (dp[x][y] = max(lcsUtils(x-1,y,s1,s2,dp), lcsUtils(x,y-1,s1,s2,dp)));
+}
 
-//int lcs(int x, int y, string s1, string s2)
-//{
-//    vector<vector<int> >dp(x+1,vector<int>(y+1, -1));
+int lcs(int x, int y, string s1, string s2)
+{
+   vector<vector<int> >dp(x+1,vector<int>(y+1, -1));
     
-//    return lcsUtils(x,y,s1,s2,dp);
-//}
-
-
+   return lcsUtils(x,y,s1,s2,dp);
+}
 
 
 // TABULATION
-
 int lcs(int x, int y, string s1, string s2)
 {
     vector<vector<int> >dp(x+1,vector<int>(y+1, 0));
@@ -61,8 +56,6 @@ int lcs(int x, int y, string s1, string s2)
     
     return dp[x][y];
 }
-
-
 
 int main()
 {
